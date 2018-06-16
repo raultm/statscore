549672e88f47323aca23d995d608821b79387629
admin <admin@admins-MacBook-Pro.local>
2018-06-16 16:06:54 +0200

    Model - Team has shortname & image

--- src/models/Team.js
+++ src/models/Team.js
@@ -3,16 +3,26 @@ import BaseModel from './BaseModel'
 export default  class Team extends BaseModel {
   constructor(params) {
     super(params)
-    const data = { club: null, season: null, ...params }
+    const data = { shortname: '', image: '', club: null, season: null, ...params }
+    this.$shortname = data.shortname
+    this.$image = data.image
     this.$club = data.club
     this.$competition = data.competition
   }
 
-   club () {
-     return this.$club
-   }
+  club () {
+    return this.$club
+  }
 
-   competition () {
-     return this.$competition
-   }
+  competition () {
+    return this.$competition
+  }
+
+  shortname () {
+    return this.$shortname
+  }
+
+  image () {
+    return this.$image
+  }
 }
--- tests/models/Team.test.js
+++ tests/models/Team.test.js
@@ -25,3 +25,19 @@ it('gets Competition given in constructor', () => {
   let team = new Team(parameters)
   expect(team.competition()).toBe(parameters.competition)
 })
+
+it('gets shortname given in constructor', () => {
+  let parameters = {
+    shortname: Math.random().toString(36).substring(7)
+  }
+  let team = new Team(parameters)
+  expect(team.shortname()).toBe(parameters.shortname)
+})
+
+it('gets image given in constructor', () => {
+  let parameters = {
+    image: Math.random().toString(36).substring(7)
+  }
+  let team = new Team(parameters)
+  expect(team.image()).toBe(parameters.image)
+})
