79133e3040fc9303821ae30a09e2c1a79c991c34
Raul Tierno <raultm@gmail.com>
2018-06-18 13:32:47 +0200

    BUG - Lib doesnt working because of async/await export regeneratorRuntime is not defined

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,10 @@
+
 console.log("Hello")
 var stats = require('../dist/main.js')
-var lib = require('./lib.js')
 
 var sinon = require('sinon')
 
 console.log(stats)
 console.log(stats.League)
-
-
-console.log(lib)
 
 console.log(sinon)
